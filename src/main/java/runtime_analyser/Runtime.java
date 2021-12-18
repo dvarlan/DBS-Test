5a4e03e4eae9da4c61759436d15c170101001556
dvarlan <95964411+dvarlan@users.noreply.github.com>
2021-12-18 12:41:22 +0100

    Update Runtime.java
    Neue einfachere Methode um die Zeit zu messen

--- src/main/java/runtime_analyser/Runtime.java
+++ src/main/java/runtime_analyser/Runtime.java
@@ -1,19 +1,18 @@
 package runtime_analyser;
 
+import java.time.Instant;
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.joda.time.LocalDateTime;
-import org.joda.time.LocalTime;
 
 public class Runtime {
 	public static void main(String args[]) {
-		String path = "C:\\Users\\PC\\Documents\\Uni\\WS 21-22\\Forschungsmodul Datenbanken\\Twitter Testdata\\Auszug json EM\\Stichproben\\recorded-cascades1623441180257.json";
+		String path = "INSERT PATH HERE";
 		
-		LocalTime timestart = LocalDateTime.now().toLocalTime(); //Startzeit
+		long unixstart = Instant.now().getEpochSecond(); //Startzeit
 		
-		SparkConf sparkConf = new SparkConf().setAppName("Stichprobentest");
+		SparkConf sparkConf = new SparkConf().setAppName("Runtimetest");
 		sparkConf.setMaster("local");
 		sparkConf.set("spark.driver.host", "localhost");
 		
@@ -21,19 +20,17 @@ public class Runtime {
 		Dataset<Row> set = spark.read().json(path);
 		set.show();
 		
-		LocalTime timeend = LocalDateTime.now().toLocalTime(); //Endzeit
-		System.out.println("Timestart: " + timestart);
-		System.out.println("Timeend: " + timeend);
-		long difhour = timeend.getHourOfDay() - timestart.getHourOfDay();
-		long difmin = timeend.getMinuteOfHour() - timestart.getMinuteOfHour();
-		long difsec = timeend.getSecondOfMinute() - timestart.getSecondOfMinute();
-		if(difsec < 0) { //Sekunden berichtigen wenn negativ
-			difmin = difmin - 1;
-			difsec = (60 - timestart.getSecondOfMinute()) + timeend.getSecondOfMinute();
-		} else if (difmin < 0) { //Minuten berichtigen wenn negativ
-			difhour = difhour - 1;
-			difmin = (60 - timestart.getMinuteOfHour()) + timeend.getSecondOfMinute();
-		}
-		System.out.println("Runtime: " + "h: " + difhour + " min: " + difmin + " s: " + difsec);
+		long unixend = Instant.now().getEpochSecond(); // Endzeit
+		
+		System.out.println("Unix Start:" + unixstart);
+		System.out.println("Unix End:" + unixend);
+		System.out.println("Unix Runtime: " + (unixend - unixstart));
+		System.out.println("Runtime in regular format: ");
+		timeConverter(unixend - unixstart);
+	}
+	
+	public static void timeConverter(long unixruntime) {
+		int x = (int) unixruntime / 60;
+			System.out.println("Stunden: " + (x / 60) + " Minuten: " + (x % 60) + " Sekunden: " + (unixruntime % 60));
 	}
 }
